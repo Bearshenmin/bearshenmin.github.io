91072aa4dc4e6f99e546b3e29b13a58645186fec
BearShen <93070462+Bearshenmin@users.noreply.github.com>
2023-06-28 17:10:04 +0800

    Update script.js

--- word_counter/script.js
+++ word_counter/script.js
@@ -5,7 +5,13 @@ textInput.addEventListener('input', updateWordCount);
 
 function updateWordCount() {
   const text = textInput.value.trim();
-  const chineseCharacters = text.replace(/[^\u4e00-\u9fa5]/g, '');
-  const wordCountText = `字數：${chineseCharacters.length}`;
+  const wordCountText = `字數：${countWords(text)}`;
   wordCount.textContent = wordCountText;
+}
+
+function countWords(text) {
+  const chineseCharacters = text.replace(/[^\u4e00-\u9fa5]/g, '');
+  const englishWords = text.match(/\b[a-zA-Z]+\b/g) || [];
+  const numbers = text.match(/\d+/g) || [];
+  return chineseCharacters.length + englishWords.length + numbers.length;
 }
